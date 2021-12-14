79f60773014160d89e9e2dc3f80c8d4989a8eac0
zhaoyr <543434337@qq.com>
2021-12-15 00:13:45 +0800

    fix: 修复node_modules下package对应的package.json点击packageName无法跳转到对应package的问题

--- src/pkgjson-dep-jump-nm.ts
+++ src/pkgjson-dep-jump-nm.ts
@@ -12,7 +12,6 @@ import { basename, dirname, join } from "path";
 import { existsSync } from "fs";
 import {
   getFileInProjectRootDir,
-  isMonorepoProject,
   genFileLocation,
   error,
 } from "./utils/index";
@@ -25,7 +24,7 @@ async function provideDefinition(
   position: Position,
   token: CancellationToken
 ): Promise<Definition | LocationLink[] | null | undefined> {
-  const filepath = document.uri.path;
+  const filepath = document.uri.fsPath;
   const fileName = basename(filepath);
   if (/package\.json$/.test(fileName)) {
     const range = document.getWordRangeAtPosition(position);
@@ -48,30 +47,22 @@ async function provideDefinition(
       const pkgName = word.replace(/"/g, "");
       const isOrganizePkg = pkgName.startsWith("@");
       const pkgNamePath = isOrganizePkg ? pkgName.split("/") : [pkgName];
-      const isMonorepo = isMonorepoProject(rootDir);
-      if (!isMonorepo) {
-        const nodeModulesPath = join(rootDir, NODE_MODULES);
-        destPath = join(nodeModulesPath, ...pkgNamePath, PACKAGE_JSON);
+      // 从package.json所在目录的node_modules寻找，直到根目录的node_modules停止。
+      let isRootDir = false;
+      let currentDirPath = filepath;
+      do {
+        currentDirPath = dirname(currentDirPath);
+        destPath = join(
+          currentDirPath,
+          NODE_MODULES,
+          ...pkgNamePath,
+          PACKAGE_JSON
+        );
         if (existsSync(destPath)) {
-          return genFileLocation(destPath);
+          return genFileLocation(destPath); // return location，字符串就会变成一个可以点击的链接
         }
-      } else {
-        let isRootFile = false;
-        let currentDirPath = filepath;
-        do {
-          currentDirPath = dirname(currentDirPath);
-          destPath = join(
-            currentDirPath,
-            NODE_MODULES,
-            ...pkgNamePath,
-            PACKAGE_JSON
-          );
-          if (existsSync(destPath)) {
-            return genFileLocation(destPath); // return location，字符串就会变成一个可以点击的链接
-          }
-          isRootFile = rootDir === currentDirPath;
-        } while (!isRootFile);
-      }
+        isRootDir = rootDir === currentDirPath;
+      } while (!isRootDir);
 
       window.showWarningMessage(t("tip.notFoundPackage"));
     }
