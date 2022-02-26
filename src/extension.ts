bdd04679c413eab681ee97aa91ccfb65e9c03675
长风 <changfeng@gaoding.com>
2022-02-27 02:36:36 +0800

    chore: revert some change

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,14 @@
 import { ExtensionContext, commands } from "vscode";
 
 export function activate(context: ExtensionContext) {
-  import('./pkgjson-dep-jump-nm').then(({ default: packageJsonJumpToNodeModules }) => {
+  import("./pkgjson-dep-jump-nm").then(({ default: packageJsonJumpToNodeModules }) => {
       packageJsonJumpToNodeModules(context);
   });
 
 
   context.subscriptions.push(
     commands.registerCommand("extension.search.package", async (uri) => {
-      import('./search-package').then(({ default: searchPackage }) => {
+      import("./search-package").then(({ default: searchPackage }) => {
           searchPackage(uri);
       });
     })
@@ -16,7 +16,7 @@ export function activate(context: ExtensionContext) {
 
   context.subscriptions.push(
     commands.registerCommand("extension.search.node_modules", async (uri) => {
-      import('./search-node_modules').then(({ default: searchNodeModules }) => {
+      import("./search-node_modules").then(({ default: searchNodeModules }) => {
           searchNodeModules(uri);
       });
     })
