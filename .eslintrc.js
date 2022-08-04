26cca128a428e68a4785a7fec0a0581a3419a382
changfeng <changfeng@gaoding.com>
2022-08-04 10:40:37 +0800

    fix: eslint semi

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module"
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
   },
-  "extends": ["prettier"],
-  "plugins": ["@typescript-eslint", "prettier"],
-  "rules": {
-    "@typescript-eslint/naming-convention": [
-      "warn",
+  extends: ['prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'warn',
       {
-        "selector": "variable",
-        "format": ["camelCase", "UPPER_CASE"],
-        "filter": {
-          "regex": "(^_)|node_modules|publisher_name",
-          "match": false
-        }
-      }
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+        filter: {
+          regex: '(^_)|node_modules|publisher_name',
+          match: false,
+        },
+      },
     ],
-    "@typescript-eslint/semi": "warn",
-    "curly": "warn",
-    "eqeqeq": "warn",
-    "no-throw-literal": "warn",
-    "semi": "off"
+    '@typescript-eslint/semi': ['error', 'never'],
+    curly: 'warn',
+    eqeqeq: 'warn',
+    'no-throw-literal': 'warn',
+    semi: 'off',
   },
-  "ignorePatterns": ["out", "dist", "**/*.d.ts"]
-};
+  ignorePatterns: ['out', 'dist', '**/*.d.ts'],
+}
